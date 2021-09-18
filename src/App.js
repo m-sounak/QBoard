006259e2e27a5d99646b76e7cac33b4a66eccbcd
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-18 12:43:06 +0530

    Test 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Main from './components/main';
+import Container from './components/container/Container'
 
 function App() {
   return (
-    <Main />
+    <Container />
   );
 }
 
