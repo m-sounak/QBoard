a49e9413302e25de7c0e93c076b2b4eeeade249b
Sounak Majumder <63659117+m-sounak@users.noreply.github.com>
2021-09-18 12:11:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-App.get("/",(req,res)=>{res.send("hi");})
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
